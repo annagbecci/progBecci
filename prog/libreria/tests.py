8df6c7907ac3cd7830685e1050826e10c402a6db
annagbecci <300105@studenti.unimore.it>
2025-09-03 15:23:05 +0200

    Pulizia dei file + piccole modifiche come aggiunta modal per nuove recensioni

--- prog/libreria/tests.py
+++ prog/libreria/tests.py
@@ -2,5 +2,5 @@ from django.test import TestCase
 
 
 class SimpleTest(TestCase):
-    def test_basic(self):
-        self.assertEqual(1 + 1, 2)
+    def test_prova(self):
+        self.assertEqual(2, (1+1))
